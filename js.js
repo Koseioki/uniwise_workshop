1fd41d9c6136dcc154cdef62a64c06151aa3f063
Kosei Oki <koseioki1225@gmail.com>
2024-09-19 12:49:27 +0200

    19/9

---
+++ js.js
@@ -0,0 +1,32 @@
+/**
+ * file: js/js.js
+ * purpose: Behaviors
+ **/
+console.log('Success: JavaScript from js/js.js running!')
+
+
+// Add this script tag to your HTML or include in your existing JavaScript file
+function showAlert() {
+    openModal();
+  }
+
+  function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+
+  function allowAction() {
+    alert("The page has now access to your location!");
+    closeModal();
+  }
+
+  function denyAction() {
+    alert("The page has NOT access to your location.");
+    closeModal();
+  }
+
+  // Call the showAlert function
+  showAlert();
